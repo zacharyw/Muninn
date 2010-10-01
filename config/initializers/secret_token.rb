5787b6a0b7bfda202b2ba0d89efb15cacb04be81
Zachary Wright <zachary.g.wright@gmail.com>
2010-09-30 21:16:30 -0400

    Initial rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muninn::Application.config.secret_token = '2836fd7c33d2caf4b81c9174590d520deb6a472f656944d67820a115facc593fd4cc1589b521a8b8ccae058a792fdb9e881699c6ff12b102453c4a71e603a6ab'
