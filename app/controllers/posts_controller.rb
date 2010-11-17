4fe9b08e6914bb6f79f8631e374bc1507c53ad8d
Zachary Wright <zachary.g.wright@gmail.com>
2010-11-16 23:53:39 -0500

    Added ability to make comments on a post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,12 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.paginate(:page => params[:page], :per_page => 3, :order => 'created_at DESC', :include => :comments)
 	end
+	
+	def show
+		@post = Post.find(params[:id])
+		@comment = Comment.new
+	end
+	
+	def add_comment
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Muninn::Application.routes.draw do
   root :to => "posts#index"
   
-  resources :posts
+  resources :posts do
+	  resources :comments
+	end
 end
