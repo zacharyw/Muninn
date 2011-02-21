5e73a57f1a791865f2e4d77737b6e4d798417b55
Zachary Wright <zachary.g.wright@gmail.com>
2011-02-21 16:43:08 -0500

    Updated standard form builder to be able to customize label.

--- app/helpers/standard_form_builder.rb
+++ app/helpers/standard_form_builder.rb
@@ -1,10 +1,20 @@
 class StandardFormBuilder < ActionView::Helpers::FormBuilder
 	def self.create_tagged_field(method_name)
 		define_method(method_name) do |label, *args|
+			
+			if(!args[0][:label].nil?)
+				label = args[0][:label]
+			end
+			
+			label_class = ""
+			if(!args[0][:label_class].nil?)
+				label_class = args[0][:label_class]
+			end
+			
 			@template.content_tag("dt",
 				@template.content_tag("label",
 															label.to_s.humanize,
-															:for => "#{@object_name}_#{label}") ) +
+															:for => "#{@object_name}_#{label}", :class => label_class) ) +
 			@template.content_tag("dd", super(label, *args))
 		end
 	end
