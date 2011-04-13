8a7cb670e8531cc788a3b987da852a6e1fa7d34b
Zachary Wright <zachary.g.wright@gmail.com>
2011-04-13 08:45:26 -0400

    Added syntaxhighlighter and google analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
 			content_tag("dl", form_for(name, *(args << options.merge(:builder => StandardFormBuilder)), &block)),
 			:class => "standard_form")
 	end
+	
+	def include_syntaxhighlighter
+		javascript_include_tag("syntaxhighlighter/shCore", "syntaxhighlighter/shBrushCss", "syntaxhighlighter/shBrushJava", "syntaxhighlighter/shBrushJScript", "syntaxhighlighter/shBrushRuby", "syntaxhighlighter/shBrushRuby", "syntaxhighlighter/shBrushSass", "syntaxhighlighter/shBrushSql", "syntaxhighlighter/shBrushXml") + stylesheet_link_tag("syntaxhighlighter/shCore", "syntaxhighlighter/shCoreDefault.css")
+	end
 end
